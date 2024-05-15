1dbeb47e1537d1a2fc15cb66d193235a1cfd02c3
Levi Eeftink <153814944+LeviEeftink@users.noreply.github.com>
2024-05-15 11:34:41 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html lang="en">
 
 <head>
+            <?php
+        include('Header/header.html');
+        ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>landingspagina</title>
