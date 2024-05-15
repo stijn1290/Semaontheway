ef7b9e717eeff0736920b8f7cd67c2273811ecfe
Levi Eeftink <153814944+LeviEeftink@users.noreply.github.com>
2024-05-15 11:30:18 +0000

    CSS Update
    1 - Betere Input Field Style
    2 - Hover Elements

---
+++ loginpage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    include 'header.html';
+    ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loginpage</title>
+    <link rel="stylesheet" href="loginpage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div>
+        <div class="first-title">
+            <h1>Sema On The Way Login</h1>
+        </div>
+    </div>
+    <div class="container">
+        <div class="logincontainer">
+            <div class="inloggen">
+                <div class="inloggen-title">
+                    <h1>Registeren</h1>
+                </div>
+                <form action="">
+                    <input id="Gebruikersnaam" placeholder="Voer uw Gebruikersnaam in.">
+                    <input id="Wachtwoord" placeholder="Voer uw Wachtwoord in.">
+                    <input type="submit" value="Submit" placeholder="Registreer">
+                </form>
+            </div>
+            <div class="inloggen">
+            <div class="inloggen-title">
+                    <h1>Inloggen</h1>
+                </div>
+                <form action="">
+                    <input id="Gebruikersnaam" placeholder="Voer uw Gebruikersnaam in.">
+                    <input id="Wachtwoord" placeholder="Voer uw Wachtwoord in.">
+                    <input type="submit" value="Submit" placeholder="Log in">
+                </form>
+            </div>
+        </div>
+        <section>
+            
+        </section>
+    </div>
+</body>
+
+</html>
