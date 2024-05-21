3e4ea90d1cfb796a70c2890fbec945e8797453dd
stijn1290 <153715940+stijn1290@users.noreply.github.com>
2024-05-21 11:28:24 +0200

    de landingspagina is toegevoegd

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html lang="en">
 
 <head>
+    <?php
+        include('Header/header.html');
+        ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>landingspagina</title>
@@ -12,28 +15,45 @@
 
 <body>
     <main>
-        <form action="" class="kolomfooter">
-            <input id="voornaam" type="text" placeholder="voornaam">
-            <input type="text" id="achternaam" placeholder="achternaam">
-            <input type="text" id="straatnaam" placeholder="straatnaam">
-            <input type="text" id="huisnummer" placeholder="huisnummer">
-            <button id="versturen" onclick="CheckField();">login</button>
-        </form>
-        <script>
-        function CheckField() {
-            var voornaam = document.getElementById("voornaam");
-            var achternaam = document.getElementById("achternaam");
-            var straatnaam = document.getElementById("straatnaam");
-            var huisnummer = document.getElementById("huisnummer");
-            if (voornaam.value === "" || achternaam.value === "" || straatnaam.value === "" || huisnummer.value === "") {
-                alert("Voer alstublieft iets in.");
-                if (isNaN(huisnummer.value)) {
-                    alert("Voer een getal in voor huisnummer.");
-                }
-            }
-            return true;
-        }
-        </script>
+        <section class="vakantiezoeken">
+            <div class="vakantieafbeelding">
+                <img src="Header/Assets/Rectangle4.png" alt="">
+                <div class="elementsinafbeelding">
+                    <h1 id="witteletters">Sema On The Way</h1>
+                    <nav class="groeneblokkenrij">
+                        <div class="zoekenblock">
+                            <h1>Zoek vakantie</h1>
+                            <form action="" class="formulierflex">
+                                <p>aantal personen</p>
+                                <input type="number" min="0" max="20">
+                                <p>aantal kamers</p>
+                                <input type="number" min="0" max="20">
+                                <p>bestemming</p>
+                                <input type="text">
+                                <p>vertrek datum</p>
+                                <input type="date">
+                                <input type="submit" value="zoek vakantie's" id="boxstyle">
+                            </form>
+                        </div>
+                        <div class="reclamekorting">
+                            <h1 id="oranjeletters">bespaar tot 150 euro korting op je vakantie</h1>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </section>
+        <section class="vakantiezoeken">
+            <div class="overonsblok">
+                <div class="overonstitel">
+                    <h1>Over Ons</h1>
+                </div>
+                <div class="overonstekst">
+                    <h1>Sema On The Way is een professioneel reisbureau dat exclusieve en hoogwaardige reizen aanbiedt.
+                        Als u op zoek bent naar een ontspannen vakantie, dan bent u bij ons aan het juiste adres!</h1>
+                        <h1>Boek hier uw vakantie!!!</h1>
+                </div>
+            </div>
+        </section>
     </main>
     <footer>
         <?php
