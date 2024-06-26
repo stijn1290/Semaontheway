46a7f3c71a94e589839469ba3eec24647d91598e
stijn1290 <153715940+stijn1290@users.noreply.github.com>
2024-06-26 13:13:51 +0200

    Update van mijnreizen.php

--- mijnreizen.php
+++ mijnreizen.php
@@ -12,7 +12,7 @@ if($_SESSION['rol'] == "user")
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loginpage</title>
-    <link rel="stylesheet" href="mijnreizen.css">
+    <link rel="stylesheet" href="assets/CSS/mijnreizen.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet">
